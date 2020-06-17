b10f15aff66511b06de67a524ff64866c55d986e
Rayjay-8 <rayarruda9876@gmail.com>
2020-06-17 06:10:14 -0300

    h

--- myapp.py
+++ myapp.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
+@app.route("/", methods=['GET'])
+def index():
+    return "<h1>Hello World!</h1>"
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=4000)
+@app.route("/deploy", methods=['GET'])    
+def deploy():
+    return "<h1>Testando deploy GitHub x Heroku </h1>"
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
